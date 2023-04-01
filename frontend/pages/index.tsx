07efcdb8c9257f75436cc638b29045817048bef5
calandula <36953570+calandula@users.noreply.github.com>
2023-04-01 23:06:09 +0200

    style changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div style={{marginTop: '300px'}}>
+    <div style={{margin: 'auto', marginTop: '300px', width: '50%'}}>
       <h1>WELCOME!</h1>
       <h2>Access to List of Users to see all the created users</h2>
       <h2>Access to Create User to create a brand new user</h2>
